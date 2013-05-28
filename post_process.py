9ba895db5d2107bc824b8bc12d084f5a562043f4
samcday <me@samcday.com.au>
2013-05-28 21:48:29 +1000

    Better phone number parsing, only 13 unparseable numbers in dataset!
    Error parsing number 09 9651 3980
    Error parsing number (0) 5331 8150
    Error parsing number 097 17 866644
    Error parsing number 039057900
    Error parsing number 6562704748
    Error parsing number 01 99222689
    Error parsing number 8282 66998
    Error parsing number 02 923505700
    Error parsing number 617322001441
    Error parsing number 075546907
    Error parsing number 08672791121
    Error parsing number 07 5573 8662 / 075573 8603
    Error parsing number 08963136200

--- post_process.py
+++ post_process.py
@@ -6,14 +6,35 @@ data = json.load(open("result.json"))
 institutions = {}
 courses = {}
 
+state_area_codes = {
+    "Australian Capital Territory": "02",
+    "New South Wales": "02",
+    "Northern Territory": "08",
+    "Queensland": "07",
+    "South Australia": "08",
+    "Tasmania": "03",
+    "Victoria": "03",
+    "Western Australia": "08"
+}
 
-def format_phone(num):
+
+def format_phone(rawnum, institution):
+    if rawnum is None:
+        return None
+    cleannum = re.sub("[^0-9]", "", rawnum)
     try:
-        num = phonenumbers.parse(num, "AU")
+        num = phonenumbers.parse(rawnum, "AU")
+        # If parse failed and number is exactly 8 digits, it's probably just
+        # missing area code.
+        if not phonenumbers.is_valid_number(num) and len(cleannum) == 8:
+            area_code = state_area_codes[institution["postal_address"]["state"]]
+            num = phonenumbers.parse(area_code + rawnum, "AU")
         if not phonenumbers.is_valid_number(num):
+            print("Error parsing number %s" % rawnum)
             return None
         return phonenumbers.format_number(num, phonenumbers.PhoneNumberFormat.NATIONAL)
     except:
+        print("Error parsing number %s" % rawnum)
         return None
 
 
@@ -39,8 +60,8 @@ for item in data:
     institution = institutions[item["institution"]]
     del item["institution"]
     if _type == "contact":
-        item["phone"] = format_phone(item["phone"])
-        item["fax"] = format_phone(item["fax"])
+        item["phone"] = format_phone(item["phone"], institution)
+        item["fax"] = format_phone(item["fax"], institution)
         institution["contacts"].append(item)
     if _type == "course":
         code = item["code"]
@@ -52,8 +73,8 @@ for item in data:
         name = item["name"]
         del item["name"]
         item["postcode"] = int(item["postcode"])
-        item["phone"] = format_phone(item["phone"])
-        item["fax"] = format_phone(item["fax"])
+        item["phone"] = format_phone(item["phone"], institution)
+        item["fax"] = format_phone(item["fax"], institution)
         institution["campuses"][name] = item
 
 json.dump(institutions, open("institutions.json", "w"))
